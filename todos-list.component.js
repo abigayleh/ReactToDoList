4c10d61facf8bf4a4a4c9b2b7bb904fbc8a01d07
abigayleh <60492560+abigayleh@users.noreply.github.com>
2021-01-13 11:02:23 -0330

    Update todos-list.component.js

--- todos-list.component.js
+++ todos-list.component.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 const Todo = props => (
     <tr>
@@ -8,42 +8,33 @@ const Todo = props => (
         <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_responsible}</td>
         <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_priority}</td>
         <td>
+
+        </td>
+        <td>
             <Link to={"/edit/"+props.todo._id}>Edit</Link>
         </td>
     </tr>
 )
 
-export default class TodosList extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            todos: []
-        }
-    }
-
-    componentDidMount(){
+export default function TodosList() {
+    const [todo, setTodo] = useState([]);
+    
+    useEffect(() => {
         axios.get('http://localhost:4000/todos/')
             .then(response => {
-                this.setState({
-                    todos: response.data
-                });
+                {setTodo(response.data)}
             })
             .catch(function(error) {
                 console.log(error)
             })
-    }
+    });
 
-    componentDidUpdate() {
-        this.componentDidMount();
-    }
-
-    todoList() {
-        return this.state.todos.map(function(currentTodo, index) {
-            return <Todo todo={currentTodo} key={index} />
+    const listTasks = () => {
+        todo.map(function(currentTodo, index){
+            return <Todo todo={currentTodo} key={index}/>
         })
     }
 
-    render() {
         return(
             <div> 
                 <br/>
@@ -54,14 +45,14 @@ export default class TodosList extends Component {
                             <th>Description</th>
                             <th>Responsible</th>
                             <th>Priority</th>
+                            <th>Completed</th>
                             <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
-                        { this.todoList() }
+                        {() => listTasks}   
                     </tbody>
                 </table>
             </div>
         )
     }
-}
